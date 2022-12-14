9c2aec2fe1f66c0a0d0a9614029b590c42ed1dfc
zzollozz <81852084+zzollozz@users.noreply.github.com>
2022-12-14 16:55:46 +0300

    save

--- Exemple_001/Program.cs
+++ Exemple_001/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Алексей");
+﻿// Это первое занятие. Знакомство с языком
+
+Console.Write("Введите Имя: "); // вывод Данных
+string username = Console.ReadLine(); // ввод Данных
+Console.Write("Привет, ");
+Console.WriteLine(username);
+
+// string - для текста (от 4 байт до 2 гб)
+// int - для целых чисел, 32-разрядное целое число
+// double - для вещественных чисел, 64-разрядное целое число
+// bool - True and False
+
+Console.WriteLine("деление случайного первого числа  на 3");
+double numberA = new Random().Next(1, 21); // рандомные числа
+double numberB = 3;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.Write("Ответ: ");
+Console.WriteLine(numberA / numberB);
+
+// Ветвление
+
+
+
+
