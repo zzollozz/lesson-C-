87b8a1e4f27f96c2bd2c6890ed4b73355928721f
zzollozz <81852084+zzollozz@users.noreply.github.com>
2022-12-14 17:37:27 +0300

    add while

--- Exemple_001/Program.cs
+++ Exemple_001/Program.cs
@@ -1,25 +1,40 @@
 ﻿// Это первое занятие. Знакомство с языком
-
+Console.WriteLine("Это первое занятие. Знакомство с языком");
 Console.Write("Введите Имя: "); // вывод Данных
 string username = Console.ReadLine(); // ввод Данных
-Console.Write("Привет, ");
-Console.WriteLine(username);
+
+// Console.WriteLine("это сторка конкатенация =>" + username);
+
 
 // string - для текста (от 4 байт до 2 гб)
 // int - для целых чисел, 32-разрядное целое число
 // double - для вещественных чисел, 64-разрядное целое число
 // bool - True and False
 
-Console.WriteLine("деление случайного первого числа  на 3");
+Console.WriteLine("Выполнения деления случайного первого числа  на 3");
 double numberA = new Random().Next(1, 21); // рандомные числа
 double numberB = 3;
-Console.WriteLine(numberA);
-Console.WriteLine(numberB);
+Console.WriteLine(numberA + " / " + numberB);
+// Console.WriteLine(numberB);
 Console.Write("Ответ: ");
 Console.WriteLine(numberA / numberB);
 
 // Ветвление
 
+if(username.ToLower() == "bob") // работа со строкой перевели в малый индекс
+{
+    Console.WriteLine("Приветствую тебя Начальник!");
+}
+else
+{
+    Console.Write("Привет, " + username);
+}
 
+// Цикл => While
 
-
+int count = 0;
+while(count < 10)
+{
+    Console.WriteLine("цикл while выполнился " + count + " раз");
+    count ++;
+}
