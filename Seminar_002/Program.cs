029bcd30b3efa57329673e923a7ea4a795e94fd0
zzollozz <81852084+zzollozz@users.noreply.github.com>
2022-12-19 11:19:24 +0300

    Практическое Второе занятие

---
+++ Seminar_002/Program.cs
@@ -0,0 +1,77 @@
+﻿// // Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// int number = new Random().Next(100, 1000);
+// int result = (number / 100) * 10 + number % 10;
+// Console.WriteLine($"{number} -> {result}");
+
+
+
+// 1. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int numb = new Random().Next(10, 100); // 34
+Console.WriteLine($"Выбранное число {numb}");
+
+if (numb / 10 > numb % 10)
+{
+    Console.WriteLine($"максимальное  значение {numb / 10}");
+}
+Console.WriteLine($"максимальное значение {numb % 10}");
+
+
+/* 2. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+  Если второе число не кратно числу первому, то программа выводит остаток от деления.
+
+34, 5 -> не кратно, остаток 4
+16, 4  -> кратно */
+
+int nun_a = int.Parse(Console.ReadLine()!);
+
+int nun_b = int.Parse(Console.ReadLine()!);
+
+int result = nun_a % nun_b;
+
+Console.WriteLine($"результат: {result} = {nun_a} % {nun_b}");
+
+if (nun_a % nun_b == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"{result}");
+}
+
+
+/* Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+14  ->  нет
+46  ->  нет
+161 ->  да */
+
+Console.WriteLine("Введите число: ");
+int nun_C = int.Parse(Console.ReadLine()!);
+
+if (nun_C % 7 == 0 && nun_C % 23 == 0)
+{
+    Console.WriteLine($"Результат кратно");
+}
+else
+{
+    Console.WriteLine($"Результат не Кратен!");
+}
+
+
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Console.WriteLine("Введите два числа: ");
+
+int number_A = int.Parse(Console.ReadLine()!);
+int number_B = int.Parse(Console.ReadLine()!);
+
+if (number_A * number_A == number_B || number_B * number_B == number_A)
+{
+    Console.WriteLine("Является");
+}
+else
+{
+    Console.WriteLine("НЕ является");
+}
